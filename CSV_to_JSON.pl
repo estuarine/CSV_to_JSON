fb496d136cfb2bf985e2006e91b14fce20f96264
Bob King <bkingdc@gmail.com>
2013-11-24 11:49:41 -0500

    Update CSV_to_JSON.pl
    Restructuring to place the processing of the command-line string into a subroutine. This could make it easier to allow the addition of more complex options later on.

--- CSV_to_JSON.pl
+++ CSV_to_JSON.pl
@@ -7,20 +7,7 @@ binmode(STDOUT, ":utf8");
 
 my $json = JSON->new;
 
-my %sepchar_options = ( "colon" => ":",
-                        "comma" => ",",
-                        "pipe"  => "|",
-                        "tab"   => "\t",
-                       );
-                      
-my $sepchar_input = $ARGV[2] || "comma";
-my $sepchar = $sepchar_options{$sepchar_input} || ",";
-
-my $infile = $ARGV[0] || 'Example.csv';
-my $IN = Tie::Handle::CSV->new($infile, header => 1, sep_char => $sepchar);
-
-my $outfile = $ARGV[1] || 'Example.json';
-open my $OUT, ">$outfile" or die "Can't open $outfile: $!\n";
+my ($IN, $infile, $OUT, $outfile) = process_input(@ARGV); 
 
 print "... Converting '$infile' to '$outfile'\n";
 
@@ -46,3 +33,30 @@ while (my $line = <$IN>) {
 my $data_json = encode_json \@data;
 
 print $OUT $data_json;
+
+exit;
+
+####################################
+
+sub process_input {
+
+    my @ARGV = @_;
+
+    my %sepchar_options = ( "colon" => ":",
+                            "comma" => ",",
+                            "pipe"  => "|",
+                            "tab"   => "\t",
+                           );
+                      
+    my $sepchar_input = $ARGV[2] || "comma";
+    my $sepchar = $sepchar_options{$sepchar_input} || ",";
+
+    my $infile = $ARGV[0] || 'Example.csv';
+    my $IN = Tie::Handle::CSV->new($infile, header => 1, sep_char => $sepchar);
+
+    my $outfile = $ARGV[1] ||  'Example.json';
+    open my $OUT, ">$outfile" or die "Can't open $outfile: $!\n";
+
+    return ($IN, $infile, $OUT, $outfile);
+
+    }
